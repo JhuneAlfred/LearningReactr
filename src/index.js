8061c3137af607cfc67276b41433105f4fa13d26
JhuneAlfred <avelicaria@me.com>
2017-09-27 00:34:04 -0700

    added index files

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import {render} from 'react-dom';
 
+
 const title= React.createElement (
   'h1',
   {id: 'title', className: 'header'},
   'hello world!'
 )
+
 
 render (
   title,
